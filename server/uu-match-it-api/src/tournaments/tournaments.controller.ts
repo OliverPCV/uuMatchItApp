6165d8dd155700408990eccad25e9b564aa393d2
Mikuláš Kopecký <mikulas@ka.cz>
2023-11-15 19:03:09 +0100

    Tournament endpoints
    - app now has identified endpoints for tournaments

--- server/uu-match-it-api/src/tournaments/tournaments.controller.ts
+++ server/uu-match-it-api/src/tournaments/tournaments.controller.ts
@@ -1,4 +1,31 @@
 import { Controller } from '@nestjs/common';
+import {Tournament} from "./tournament.interface";
 
 @Controller('tournaments')
-export class TournamentsController {}
+export class TournamentsController {
+
+
+    createTournament(tournament: Tournament) {
+        throw new Error("Method not implemented.");
+    }
+
+    getTournaments() {
+        throw new Error("Method not implemented.");
+    }
+
+
+    getTournamentDetail(id: string) {
+
+    }
+
+    editTournament(id: string, tournament: Tournament) {
+        throw new Error("Method not implemented.");
+    }
+
+    createMatches(id: string) {
+        throw new Error("Method not implemented.");
+    }
+
+
+
+}
