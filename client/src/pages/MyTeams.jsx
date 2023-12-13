d1ca1af3c34ef71b1c52b36cbfa1090407c06168
OliverPCV <oliver.cavallo@gmail.com>
2023-12-13 18:32:48 +0100

    update myteams

--- client/src/pages/MyTeams.jsx
+++ client/src/pages/MyTeams.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import { teams } from '../data/teams';
+import { Button } from 'react-bootstrap';
+import "../styles/page-style/MyTeams.css"
 
 function MyTeams() {
 
   return (
-    <div>
+    <div className='myteams-container'>
       <h3>Moje týmy:</h3>
-      {teams.length > 0 ? (
-        teams.map(team => (
-          <h1 key={team.id}>{team.name}</h1>
-        ))
-      ) : (
-        <p>Nemáte žádné týmy.</p>
-      )}
+      <div className="my-teams">
+        {teams.length > 0 ? (
+          teams.map(team => (
+
+            <div class="card-group">
+              <div class="card">
+                <img src="https://www.saturdayfootball.org/uploads/2/9/9/8/2998227/team-kh_orig.jpg" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">{team.name}</h5>
+                  <Button variant="outline-success" >Detail</Button>
+
+                </div>
+              </div>
+
+            </div>
+          ))
+        ) : (
+          <p>Nemáte žádné týmy.</p>
+        )}
+      </div>
+
     </div>
   );
 }
