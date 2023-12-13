fb2ba513123b5da5fb43b3678c0e7f1917c333e8
Mikuláš Kopecký <mikulas@ka.cz>
2023-12-13 17:48:17 +0100

    Tournament entity for database access

--- server/uu-match-it-api/src/entities/tournament.entity.ts
+++ server/uu-match-it-api/src/entities/tournament.entity.ts
@@ -1,5 +1,23 @@
-import {Entity} from "typeorm";
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { UserEntity } from './user.entity';
 
 
 @Entity()
-export class TournamentEntity {}
+export class TournamentEntity {
+
+
+  constructor() {
+  }
+
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  ownerId: string;
+
+  @ManyToMany(() => UserEntity)
+  @JoinTable() players: UserEntity[];
+}
