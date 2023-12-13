98b8b94687fd2b64b668f87e1b822d960d156bc0
Zemlaman <matej.zemla@seznam.cz>
2023-12-13 17:59:13 +0100

    up

---
+++ client/src/data/teams.js
@@ -0,0 +1,22 @@
+export const teams = [
+    {
+        id: 1,
+        name: "FC Postižín",
+        ownerId: 1,
+        players: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    },
+    {
+        id: 2,
+        name: "SK Úvaly",
+        ownerId: 1,
+        players: [10, 11, 12, 13, 14, 15, 16, 17, 18],
+    },
+    {
+        id: 3,
+        name: "FC NEKOPNEMSI",
+        ownerId: 2,
+        players: [10, 11, 12, 13, 14, 15, 16, 17, 18],
+    },
+]
+
+export default teams;
--- client/src/pages/MyTeams.jsx
+++ client/src/pages/MyTeams.jsx
@@ -1,33 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from '../data/user';
+import React from 'react';
 import { teams } from '../data/teams';
-import { Button } from 'react-bootstrap';
 
-function MyTeams({ loggedIn }) {
-  const [id, setId] = useState('');
-
-  useEffect(() => {
-    if (loggedIn) {
-      setId(mockUser.id);
-    }
-  }, [loggedIn]);
-
-  if (!loggedIn) {
-    return (
-      <div>
-        <Button variant="primary">Přihlásit se</Button>
-        <Button variant="secondary">Registrovat se</Button>
-      </div>
-    );
-  }
-
-  const userTeams = teams.filter(team => team.id === id);
+function MyTeams() {
 
   return (
     <div>
       <h3>Moje týmy:</h3>
-      {userTeams.length > 0 ? (
-        userTeams.map(team => (
+      {teams.length > 0 ? (
+        teams.map(team => (
           <h1 key={team.id}>{team.name}</h1>
         ))
       ) : (
