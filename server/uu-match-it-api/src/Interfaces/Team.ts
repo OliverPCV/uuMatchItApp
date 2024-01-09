b05c511a3fc740d0185f75c25fcc5709c0a97fdc
Mikuláš Kopecký <mikulas@ka.cz>
2024-01-09 15:01:21 +0100

    Teams database connection
    - now properly uses the database

--- server/uu-match-it-api/src/Interfaces/Team.ts
+++ server/uu-match-it-api/src/Interfaces/Team.ts
@@ -3,6 +3,12 @@ import { User } from './User';
 
 @Entity()
 export class Team {
+
+  constructor(name: string, owner: User) {
+    this.name = name;
+    this.owner = owner;
+  }
+
   @PrimaryGeneratedColumn() id: number;
   @Column() name: string;
 
--- server/uu-match-it-api/src/teams/teams.service.ts
+++ server/uu-match-it-api/src/teams/teams.service.ts
@@ -1,52 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { Team } from '../Interfaces/Team';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class TeamsService {
-
-
-  constructor() {
+  constructor(@InjectRepository(Team) private teamRep: Repository<Team>) {
   }
 
-  private teams: Team[] = [
-    {
-      id: 1,
-      name: "FC Postižín",
-      ownerId: 1,
-      players: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    },
-    {
-      id: 2,
-      name: "SK Úvaly",
-      ownerId: 1,
-      players: [10, 11, 12, 13, 14, 15, 16, 17, 18],
-    },
-    {
-      id: 3,
-      name: "FC NEKOPNEMSI",
-      ownerId: 2,
-      players: [10, 11, 12, 13, 14, 15, 16, 17, 18],
-    },
-  ]
-
-  private idCounter: number = 3;
-
-
-
-  getTeam(id: string) {
-    return this.teams.find(team => team.id == parseInt(id));
+  getTeam(id: number) {
+    return this.teamRep.findOneBy({ id: id });
   }
 
-  createTeam(id: string, name: string) {
-    this.teams.push({
-      id: ++this.idCounter,
-      name: name,
-      ownerId: parseInt(id),
-      players: [],
-    })
+  async createTeam(team: Team) {
+    return this.teamRep.save(team);
   }
 
   getTeams() {
-    return this.teams;
+    return this.teamRep.find();
   }
 }
