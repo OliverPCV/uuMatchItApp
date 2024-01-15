e792e8278d0e32841ccddda6e5fc6567b272fa8e
unknown <oliver.cavallo@educanet.cz>
2024-01-15 23:17:27 +0100

    update

--- client/src/pages/TournamentDetail.jsx
+++ client/src/pages/TournamentDetail.jsx
@@ -96,13 +96,16 @@ function TournamentDetail() {
       <div className="header-image">
         <div className="header-text">
           <h1 className="tournament-detail-title text">{tournament.name}</h1>
-          {userTeams.length > 0 && tournament.teams.length < tournament.sizeLimit && (
-            <button className="register-button text" onClick={() => setShowModal(true)}>Zapsat tým</button>
-          )}
-          {
-            actualUser.id === tournament.owner.id && (
-              <button className="delete-button text" onClick={() => handleDeleteTournament(tournament.id)}>Smazat turnaj</button>
-          )}
+          <div className='buttons-tour'>
+            {userTeams.length > 0 && tournament.teams.length < tournament.sizeLimit && (
+              <button className="register-button text" onClick={() => setShowModal(true)}>Zapsat tým</button>
+            )}
+            {
+              actualUser.id === tournament.owner.id && (
+                <button className="delete-button text" onClick={() => handleDeleteTournament(tournament.id)}>Smazat turnaj</button>
+              )}
+          </div>
+
         </div>
       </div>
       <Container className="tournament-detail-container">
